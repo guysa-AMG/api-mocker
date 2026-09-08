ef3a0c7ecd88f56e695accb0df89709cfbdb7e98
guysa-AMG <moosathejoker@gmail.com>
2026-09-08 15:32:46 +0200

    added fastapi

---
+++ src/api_mocker/service/api_service.py
@@ -0,0 +1,6 @@
+
+
+class Api_Server:
+    
+    def handle_route(self):
+        pass
--- src/api_mocker/service/mock_engine.py
+++ src/api_mocker/service/mock_engine.py
@@ -2,13 +2,21 @@ from src.api_mocker.interface.Imock_engine import IEphemeralMockEngine
 from typing import Dict, Any, Optional,Tuple
 
 class MockEngine(IEphemeralMockEngine):
+    _registered_specs: Dict
     def __init__(self):
         super().__init__()
+        self._registered_specs = Dict();
 
     def register_spec(self, project_id: str, spec_dict: Dict[str, Any]) -> None:
-        pass
+        if not self.validate_spec(spec_dict):
+            return
         
-    
+        self._registered_specs[project_id] = spec_dict
+        
+    def validate_spec(spec_dict: Dict[str, Any]) -> bool:
+        # TODO Implement check logic
+        return True
+        
  
     def handle_request(
         self,
